5e11800598daa7680e1223beb5392b05c17926de
cschaefer26 <c.schaefer.home@gmail.com>
2021-05-05 17:27:43 +0200

    Fixed filtering in preprocessing.

--- preprocess.py
+++ preprocess.py
@@ -84,7 +84,7 @@ if __name__ == '__main__':
 
     text_dict = ljspeech(path)
     text_dict = {item_id: text for item_id, text in text_dict.items()
-                 if item_id in wav_ids and len(text) > 2}
+                 if item_id in wav_ids and len(text) > 1}
     wav_files = [w for w in wav_files if w.stem in text_dict]
     print(f'Using {len(wav_files)} wav files that are indexed in metafile.\n')
 
