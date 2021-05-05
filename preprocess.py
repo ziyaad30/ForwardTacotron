12a7db8f4377249c986dbc59f4f0a43c1d82f593
cschaefer26 <c.schaefer.home@gmail.com>
2021-05-05 17:27:33 +0200

    Fixed filtering in preprocessing.

--- preprocess.py
+++ preprocess.py
@@ -83,7 +83,8 @@ if __name__ == '__main__':
     assert len(wav_files) > 0, f'Found no wav files in {path}, exiting.'
 
     text_dict = ljspeech(path)
-    text_dict = {item_id: text for item_id, text in text_dict.items() if item_id in wav_ids}
+    text_dict = {item_id: text for item_id, text in text_dict.items()
+                 if item_id in wav_ids and len(text) > 2}
     wav_files = [w for w in wav_files if w.stem in text_dict]
     print(f'Using {len(wav_files)} wav files that are indexed in metafile.\n')
 
