2db8d38d93efef6169ed347ea1aa3bf7fbae7bea
cschaefer26 <c.schaefer.home@gmail.com>
2021-05-19 09:07:32 +0200

    Impl zoneout.

--- trainer/forward_trainer.py
+++ trainer/forward_trainer.py
@@ -71,14 +71,22 @@ class ForwardTrainer:
                 start = time.time()
                 model.train()
 
+                pitch_zoneout_mask = torch.rand(batch['x'].size()) > self.train_cfg['pitch_zoneout']
+                energy_zoneout_mask = torch.rand(batch['x'].size()) > self.train_cfg['energy_zoneout']
+
+                pitch_target = batch['pitch'].detach().clone()
+                energy_target = batch['energy'].detach().clone()
+                batch['pitch'] = batch['pitch'] * pitch_zoneout_mask.to(device).float()
+                batch['energy'] = batch['energy'] * energy_zoneout_mask.to(device).float()
+
                 pred = model(batch)
 
                 m1_loss = self.l1_loss(pred['mel'], batch['mel'], batch['mel_len'])
                 m2_loss = self.l1_loss(pred['mel_post'], batch['mel'], batch['mel_len'])
 
                 dur_loss = self.l1_loss(pred['dur'].unsqueeze(1), batch['dur'].unsqueeze(1), batch['x_len'])
-                pitch_loss = self.l1_loss(pred['pitch'], batch['pitch'].unsqueeze(1), batch['x_len'])
-                energy_loss = self.l1_loss(pred['energy'], batch['energy'].unsqueeze(1), batch['x_len'])
+                pitch_loss = self.l1_loss(pred['pitch'], pitch_target.unsqueeze(1), batch['x_len'])
+                energy_loss = self.l1_loss(pred['energy'], energy_target.unsqueeze(1), batch['x_len'])
 
                 loss = m1_loss + m2_loss \
                        + self.train_cfg['dur_loss_factor'] * dur_loss \
