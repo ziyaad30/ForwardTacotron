16ae51c09c4015a5bc164c9949cad89e2bd17bd4
cschaefer26 <c.schaefer.home@gmail.com>
2021-06-17 13:31:32 +0200

    Added jit generation function.

--- models/common_layers.py
+++ models/common_layers.py
@@ -13,7 +13,7 @@ class HighwayNetwork(nn.Module):
         self.W2 = nn.Linear(size, size)
         self.W1.bias.data.fill_(0.)
 
-    def forward(self, x: torch.tensor) -> torch.tensor:
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         x1 = self.W1(x)
         x2 = self.W2(x)
         g = torch.sigmoid(x2)
@@ -32,7 +32,7 @@ class BatchNormConv(nn.Module):
         self.bnorm = nn.BatchNorm1d(out_channels)
         self.relu = relu
 
-    def forward(self, x: torch.tensor) -> torch.tensor:
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.conv(x)
         x = F.relu(x) if self.relu is True else x
         return self.bnorm(x)
@@ -48,9 +48,6 @@ class CBHG(nn.Module):
                  num_highways: int) -> None:
         super().__init__()
 
-        # List of all rnns to call `flatten_parameters()` on
-        self._to_flatten = []
-
         self.bank_kernels = [i for i in range(1, K + 1)]
         self.conv1d_bank = nn.ModuleList()
         for k in self.bank_kernels:
@@ -62,31 +59,15 @@ class CBHG(nn.Module):
         self.conv_project1 = BatchNormConv(len(self.bank_kernels) * channels, proj_channels[0], 3)
         self.conv_project2 = BatchNormConv(proj_channels[0], proj_channels[1], 3, relu=False)
 
-        # Fix the highway input if necessary
-        if proj_channels[-1] != channels:
-            self.highway_mismatch = True
-            self.pre_highway = nn.Linear(proj_channels[-1], channels, bias=False)
-        else:
-            self.highway_mismatch = False
-
+        self.pre_highway = nn.Linear(proj_channels[-1], channels, bias=False)
         self.highways = nn.ModuleList()
         for i in range(num_highways):
             hn = HighwayNetwork(channels)
             self.highways.append(hn)
 
         self.rnn = nn.GRU(channels, channels, batch_first=True, bidirectional=True)
-        self._to_flatten.append(self.rnn)
 
-        # Avoid fragmentation of RNN parameters and associated warning
-        self._flatten_parameters()
-
-    def forward(self, x: torch.tensor) -> torch.tensor:
-        # Although we `_flatten_parameters()` on init, when using DataParallel
-        # the model gets replicated, making it no longer guaranteed that the
-        # weights are contiguous in GPU memory. Hence, we must call it again
-        self._flatten_parameters()
-
-        # Save these for later
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         residual = x
         seq_len = x.size(-1)
         conv_bank = []
@@ -111,15 +92,10 @@ class CBHG(nn.Module):
 
         # Through the highways
         x = x.transpose(1, 2)
-        if self.highway_mismatch is True:
-            x = self.pre_highway(x)
-        for h in self.highways: x = h(x)
+        x = self.pre_highway(x)
+        for h in self.highways:
+            x = h(x)
 
         # And then the RNN
         x, _ = self.rnn(x)
         return x
-
-    def _flatten_parameters(self):
-        """Calls `flatten_parameters` on all the rnns used by the WaveRNN. Used
-        to improve efficiency and avoid PyTorch yelling at us."""
-        [m.flatten_parameters() for m in self._to_flatten]
--- models/forward_tacotron.py
+++ models/forward_tacotron.py
@@ -5,13 +5,12 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from dp.utils.io import read_config
 from torch.nn import Embedding
-from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence, pad_sequence
 
 from models.common_layers import CBHG
 from utils.text.symbols import phonemes
-
-MEL_PAD_VALUE = -11.5129
 
 
 class LengthRegulator(nn.Module):
@@ -19,29 +18,13 @@ class LengthRegulator(nn.Module):
     def __init__(self):
         super().__init__()
 
-    def forward(self, x, dur):
-        return self.expand(x, dur)
-
-    @staticmethod
-    def build_index(duration: torch.tensor, x: torch.tensor) -> torch.tensor:
-        duration[duration < 0] = 0
-        tot_duration = duration.cumsum(1).detach().cpu().numpy().astype('int')
-        max_duration = int(tot_duration.max().item())
-        index = np.zeros([x.shape[0], max_duration, x.shape[2]], dtype='long')
-
-        for i in range(tot_duration.shape[0]):
-            pos = 0
-            for j in range(tot_duration.shape[1]):
-                pos1 = tot_duration[i, j]
-                index[i, pos:pos1, :] = j
-                pos = pos1
-            index[i, pos:, :] = j
-        return torch.LongTensor(index).to(duration.device)
-
-    def expand(self, x: torch.tensor, dur: torch.tensor) -> torch.tensor:
-        idx = self.build_index(dur, x)
-        y = torch.gather(x, 1, idx)
-        return y
+    def forward(self, x: torch.Tensor, dur: torch.Tensor) -> torch.Tensor:
+        x_expanded = []
+        for i in range(x.size(0)):
+            x_exp = torch.repeat_interleave(x[i], (dur[i] + 0.5).long(), dim=0)
+            x_expanded.append(x_exp)
+        x_expanded = pad_sequence(x_expanded, padding_value=0., batch_first=True)
+        return x_expanded
 
 
 class SeriesPredictor(nn.Module):
@@ -50,67 +33,40 @@ class SeriesPredictor(nn.Module):
         super().__init__()
         self.embedding = Embedding(num_chars, emb_dim)
         self.convs = torch.nn.ModuleList([
-            BatchNormConv(emb_dim, conv_dims, 5, activation=torch.relu),
-            BatchNormConv(conv_dims, conv_dims, 5, activation=torch.relu),
-            BatchNormConv(conv_dims, conv_dims, 5, activation=torch.relu),
+            BatchNormConv(emb_dim, conv_dims, 5, relu=True),
+            BatchNormConv(conv_dims, conv_dims, 5, relu=True),
+            BatchNormConv(conv_dims, conv_dims, 5, relu=True),
         ])
         self.rnn = nn.GRU(conv_dims, rnn_dims, batch_first=True, bidirectional=True)
         self.lin = nn.Linear(2 * rnn_dims, 1)
         self.dropout = dropout
 
     def forward(self,
-                x: torch.tensor,
-                x_lens: torch.tensor = None,
-                alpha=1.0) -> torch.tensor:
+                x: torch.Tensor,
+                alpha: float = 1.0) -> torch.Tensor:
         x = self.embedding(x)
         x = x.transpose(1, 2)
         for conv in self.convs:
             x = conv(x)
             x = F.dropout(x, p=self.dropout, training=self.training)
         x = x.transpose(1, 2)
-        if x_lens is not None:
-            x = pack_padded_sequence(x, lengths=x_lens, batch_first=True,
-                                     enforce_sorted=False)
         x, _ = self.rnn(x)
-        if x_lens is not None:
-            x, _ = pad_packed_sequence(x, padding_value=0.0, batch_first=True)
         x = self.lin(x)
         return x / alpha
 
 
-class ConvResNet(nn.Module):
-
-    def __init__(self, in_dims: int, conv_dims=256) -> None:
-        super().__init__()
-        self.first_conv = BatchNormConv(in_dims, conv_dims, 5, activation=torch.relu)
-        self.convs = torch.nn.ModuleList([
-            BatchNormConv(conv_dims, conv_dims, 5, activation=torch.relu),
-            BatchNormConv(conv_dims, conv_dims, 5, activation=torch.relu),
-        ])
-
-    def forward(self, x: torch.tensor) -> torch.tensor:
-        x = x.transpose(1, 2)
-        x = self.first_conv(x)
-        for conv in self.convs:
-            x_res = x
-            x = conv(x)
-            x = x_res + x
-        x = x.transpose(1, 2)
-        return x
-
-
 class BatchNormConv(nn.Module):
 
-    def __init__(self, in_channels: int, out_channels: int, kernel: int, activation=None):
+    def __init__(self, in_channels: int, out_channels: int, kernel: int, relu: bool = False):
         super().__init__()
         self.conv = nn.Conv1d(in_channels, out_channels, kernel, stride=1, padding=kernel // 2, bias=False)
         self.bnorm = nn.BatchNorm1d(out_channels)
-        self.activation = activation
+        self.relu = relu
 
-    def forward(self, x: torch.tensor) -> torch.tensor:
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.conv(x)
-        if self.activation:
-            x = self.activation(x)
+        if self.relu:
+            x = F.relu(x)
         x = self.bnorm(x)
         return x
 
@@ -190,21 +146,20 @@ class ForwardTacotron(nn.Module):
                 nn.Conv1d(1, energy_emb_dims, kernel_size=3, padding=1),
                 nn.Dropout(energy_proj_dropout))
 
-    def forward(self, batch: Dict[str, torch.tensor]) -> Dict[str, torch.tensor]:
+    def forward(self, batch: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
         x = batch['x']
         mel = batch['mel']
         dur = batch['dur']
         mel_lens = batch['mel_len']
-        x_lens = batch['x_len'].cpu()
         pitch = batch['pitch'].unsqueeze(1)
         energy = batch['energy'].unsqueeze(1)
 
         if self.training:
             self.step += 1
 
-        dur_hat = self.dur_pred(x, x_lens=x_lens).squeeze(-1)
-        pitch_hat = self.pitch_pred(x, x_lens=x_lens).transpose(1, 2)
-        energy_hat = self.energy_pred(x, x_lens=x_lens).transpose(1, 2)
+        dur_hat = self.dur_pred(x).squeeze(-1)
+        pitch_hat = self.pitch_pred(x).transpose(1, 2)
+        energy_hat = self.energy_pred(x).transpose(1, 2)
 
         x = self.embedding(x)
         x = x.transpose(1, 2)
@@ -227,7 +182,7 @@ class ForwardTacotron(nn.Module):
 
         x, _ = self.lstm(x)
 
-        x, _ = pad_packed_sequence(x, padding_value=MEL_PAD_VALUE, batch_first=True)
+        x, _ = pad_packed_sequence(x, padding_value=-11.5129, batch_first=True)
 
         x = F.dropout(x,
                       p=self.dropout,
@@ -246,12 +201,10 @@ class ForwardTacotron(nn.Module):
                 'dur': dur_hat, 'pitch': pitch_hat, 'energy': energy_hat}
 
     def generate(self,
-                 x: torch.tensor,
+                 x: torch.Tensor,
                  alpha=1.0,
-                 pitch_function: Callable[[torch.tensor], torch.tensor] = lambda x: x,
-                 energy_function: Callable[[torch.tensor], torch.tensor] = lambda x: x,
-
-                 ) -> Dict[str, np.array]:
+                 pitch_function: Callable[[torch.Tensor], torch.Tensor] = lambda x: x,
+                 energy_function: Callable[[torch.Tensor], torch.Tensor] = lambda x: x) -> Dict[str, np.array]:
         self.eval()
 
         dur = self.dur_pred(x, alpha=alpha)
@@ -300,9 +253,54 @@ class ForwardTacotron(nn.Module):
         return {'mel': x, 'mel_post': x_post, 'dur': dur,
                 'pitch': pitch_hat, 'energy': energy_hat}
 
-    def pad(self, x: torch.tensor, max_len: int) -> torch.tensor:
+    @torch.jit.export
+    def generate_jit(self,
+                     x: torch.Tensor,
+                     alpha: float = 1.0) -> Dict[str, torch.Tensor]:
+
+        dur = self.dur_pred(x, alpha=alpha)
+        dur = dur.squeeze(2)
+
+        # Fixing breaking synth of silent texts
+        if torch.sum(dur.long()) <= 0:
+            dur = torch.full(x.size(), fill_value=2, device=x.device)
+
+        pitch_hat = self.pitch_pred(x).transpose(1, 2)
+        energy_hat = self.energy_pred(x).transpose(1, 2)
+
+        x = self.embedding(x)
+        x = x.transpose(1, 2)
+        x = self.prenet(x)
+
+        if self.pitch_emb_dims > 0:
+            pitch_hat_proj = self.pitch_proj(pitch_hat).transpose(1, 2)
+            x = torch.cat([x, pitch_hat_proj], dim=-1)
+
+        if self.energy_emb_dims > 0:
+            energy_hat_proj = self.energy_proj(energy_hat).transpose(1, 2)
+            x = torch.cat([x, energy_hat_proj], dim=-1)
+
+        x = self.lr(x, dur)
+
+        x, _ = self.lstm(x)
+        x = F.dropout(x,
+                      p=self.dropout,
+                      training=self.training)
+        x = self.lin(x)
+        x = x.transpose(1, 2)
+
+        x_post = self.postnet(x)
+        x_post = self.post_proj(x_post)
+        x_post = x_post.transpose(1, 2)
+
+        x, x_post, dur = x.squeeze(), x_post.squeeze(), dur.squeeze()
+
+        return {'mel': x, 'mel_post': x_post, 'dur': dur,
+                'pitch': pitch_hat, 'energy': energy_hat}
+
+    def pad(self, x: torch.Tensor, max_len: int) -> torch.Tensor:
         x = x[:, :, :max_len]
-        x = F.pad(x, [0, max_len - x.size(2), 0, 0], 'constant', MEL_PAD_VALUE)
+        x = F.pad(x, [0, max_len - x.size(2), 0, 0], 'constant', -11.5129)
         return x
 
     def get_step(self) -> int:
@@ -321,3 +319,15 @@ class ForwardTacotron(nn.Module):
         model = ForwardTacotron.from_config(checkpoint['config'])
         model.load_state_dict(checkpoint['model'])
         return model
+
+
+if __name__ == '__main__':
+    config = read_config('../config.yaml')
+    tts_model = ForwardTacotron.from_config(config)
+    tts_model.eval()
+
+    model_script = torch.jit.script(tts_model)
+
+    x = torch.ones((1, 5)).long()
+    y = model_script.generate_jit(x)
+    print(y)
