8f1e6e745d1cfe10cd4e8229790354744e8103a1
cschaefer26 <c.schaefer.home@gmail.com>
2021-06-24 16:50:23 +0200

    transformer conv.

--- models/forward_tacotron.py
+++ models/forward_tacotron.py
@@ -1,12 +1,12 @@
 from pathlib import Path
-from typing import Union, Callable, Dict, Any
+from typing import Union, Callable, Dict, Any, Optional
 
 import math
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.nn import Embedding, TransformerEncoderLayer, LayerNorm, TransformerEncoder
+from torch.nn import Embedding, LayerNorm, TransformerEncoder, MultiheadAttention
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence, pad_sequence
 
 from models.common_layers import CBHG
@@ -58,6 +58,68 @@ def make_len_mask(inp: torch.Tensor) -> torch.Tensor:
     return (inp == 0).transpose(0, 1)
 
 
+class TransformerEncoderConvLayer(nn.Module):
+    r"""TransformerEncoderLayer is made up of self-attn and feedforward network.
+    This standard encoder layer is based on the paper "Attention Is All You Need".
+    Ashish Vaswani, Noam Shazeer, Niki Parmar, Jakob Uszkoreit, Llion Jones, Aidan N Gomez,
+    Lukasz Kaiser, and Illia Polosukhin. 2017. Attention is all you need. In Advances in
+    Neural Information Processing Systems, pages 6000-6010. Users may modify or implement
+    in a different way during application.
+
+    Args:
+        d_model: the number of expected features in the input (required).
+        nhead: the number of heads in the multiheadattention models (required).
+        dim_feedforward: the dimension of the feedforward network model (default=2048).
+        dropout: the dropout value (default=0.1).
+        activation: the activation function of intermediate layer, relu or gelu (default=relu).
+
+    Examples::
+        >>> encoder_layer = nn.TransformerEncoderLayer(d_model=512, nhead=8)
+        >>> src = torch.rand(10, 32, 512)
+        >>> out = encoder_layer(src)
+    """
+
+    def __init__(self, d_model, nhead, dim_feedforward=2048, dropout=0.1):
+        super(TransformerEncoderConvLayer, self).__init__()
+        self.self_attn = MultiheadAttention(d_model, nhead, dropout=dropout)
+        # Implementation of Feedforward model
+        self.linear1 = nn.Linear(d_model, dim_feedforward)
+        self.dropout = nn.Dropout(dropout)
+        self.conv = nn.Conv1d(dim_feedforward, d_model, 3, stride=1, padding=1)
+
+        self.norm1 = LayerNorm(d_model)
+        self.norm2 = LayerNorm(d_model)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+
+        self.activation = torch.nn.ReLU()
+
+    def __setstate__(self, state):
+        if 'activation' not in state:
+            state['activation'] = F.relu
+        super(TransformerEncoderConvLayer, self).__setstate__(state)
+
+    def forward(self, src: torch.Tensor, src_mask: Optional[torch.Tensor] = None, src_key_padding_mask: Optional[torch.Tensor] = None) -> torch.Tensor:
+        r"""Pass the input through the encoder layer.
+
+        Args:
+            src: the sequence to the encoder layer (required).
+            src_mask: the mask for the src sequence (optional).
+            src_key_padding_mask: the mask for the src keys per batch (optional).
+
+        Shape:
+            see the docs in Transformer class.
+        """
+        src2 = self.self_attn(src, src, src, attn_mask=src_mask,
+                              key_padding_mask=src_key_padding_mask)[0]
+        src = src + self.dropout1(src2)
+        src = self.norm1(src)
+        src2 = self.conv(self.dropout(self.activation(self.linear1(src))).transpose(1, 2)).transpose(1, 2)
+        src = src + self.dropout2(src2)
+        src = self.norm2(src)
+        return src
+
+
 class ForwardTransformer(torch.nn.Module):
 
     def __init__(self,
@@ -74,11 +136,10 @@ class ForwardTransformer(torch.nn.Module):
         self.embedding = nn.Embedding(encoder_vocab_size, d_model)
         self.pos_encoder = PositionalEncoding(d_model, dropout)
 
-        encoder_layer = TransformerEncoderLayer(d_model=d_model,
-                                                nhead=heads,
-                                                dim_feedforward=d_fft,
-                                                dropout=dropout,
-                                                activation='relu')
+        encoder_layer = TransformerEncoderConvLayer(d_model=d_model,
+                                                    nhead=heads,
+                                                    dim_feedforward=d_fft,
+                                                    dropout=dropout)
         encoder_norm = LayerNorm(d_model)
         self.encoder = TransformerEncoder(encoder_layer=encoder_layer,
                                           num_layers=layers,
