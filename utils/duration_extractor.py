b847a5fe04084468ccaf39299ae9d40a728c82f7
cschafer26 <c.schaefer.home@gmail.com>
2022-05-12 12:56:09 +0200

    Minor refactoring.

--- utils/duration_extractor.py
+++ utils/duration_extractor.py
@@ -8,52 +8,6 @@ from scipy.sparse.csgraph import dijkstra
 from utils.text.symbols import silent_phonemes_indices
 
 
-def to_node_index(i: int, j: int, cols: int) -> int:
-    return cols * i + j
-
-
-def from_node_index(node_index: int, cols: int) -> Tuple[int, int]:
-    return node_index // cols, node_index % cols
-
-
-def to_adj_matrix(mat: np.array) -> csr_matrix:
-    rows = mat.shape[0]
-    cols = mat.shape[1]
-
-    row_ind = []
-    col_ind = []
-    data = []
-
-    for i in range(rows):
-        for j in range(cols):
-
-            node = to_node_index(i, j, cols)
-
-            if j < cols - 1:
-                right_node = to_node_index(i, j + 1, cols)
-                weight_right = mat[i, j + 1]
-                row_ind.append(node)
-                col_ind.append(right_node)
-                data.append(weight_right)
-
-            if i < rows - 1 and j < cols:
-                bottom_node = to_node_index(i + 1, j, cols)
-                weight_bottom = mat[i + 1, j]
-                row_ind.append(node)
-                col_ind.append(bottom_node)
-                data.append(weight_bottom)
-
-            if i < rows - 1 and j < cols - 1:
-                bottom_right_node = to_node_index(i + 1, j + 1, cols)
-                weight_bottom_right = mat[i + 1, j + 1]
-                row_ind.append(node)
-                col_ind.append(bottom_right_node)
-                data.append(weight_bottom_right)
-
-    adj_mat = coo_matrix((data, (row_ind, col_ind)), shape=(rows * cols, rows * cols))
-    return adj_mat.tocsr()
-
-
 class DurationExtractor:
 
     def __init__(self,
@@ -97,7 +51,7 @@ class DurationExtractor:
 
         att = torch.clamp(att, min=0., max=1.)
         path_probs = 1.-att[:mel_len, :]
-        adj_matrix = to_adj_matrix(path_probs)
+        adj_matrix = self._to_adj_matrix(path_probs)
         dist_matrix, predecessors = dijkstra(csgraph=adj_matrix, directed=True,
                                              indices=0, return_predecessors=True)
         path = []
@@ -117,7 +71,7 @@ class DurationExtractor:
 
         # collect indices (mel, text) along the path
         for node_index in path:
-            i, j = from_node_index(node_index, cols)
+            i, j = self._from_node_index(node_index, cols)
             mel_text[i] = j
             if not sil_mask[i]:
                 att_scores.append(float(att[i, j]))
@@ -128,3 +82,49 @@ class DurationExtractor:
         att_score = sum(att_scores) / len(att_scores)
 
         return durations, att_score
+
+    @staticmethod
+    def _to_node_index(i: int, j: int, cols: int) -> int:
+        return cols * i + j
+
+    @staticmethod
+    def _from_node_index(node_index: int, cols: int) -> Tuple[int, int]:
+        return node_index // cols, node_index % cols
+
+    @staticmethod
+    def _to_adj_matrix(mat: np.array) -> csr_matrix:
+        rows = mat.shape[0]
+        cols = mat.shape[1]
+
+        row_ind = []
+        col_ind = []
+        data = []
+
+        for i in range(rows):
+            for j in range(cols):
+
+                node = DurationExtractor._to_node_index(i, j, cols)
+
+                if j < cols - 1:
+                    right_node = DurationExtractor._to_node_index(i, j + 1, cols)
+                    weight_right = mat[i, j + 1]
+                    row_ind.append(node)
+                    col_ind.append(right_node)
+                    data.append(weight_right)
+
+                if i < rows - 1 and j < cols:
+                    bottom_node = DurationExtractor._to_node_index(i + 1, j, cols)
+                    weight_bottom = mat[i + 1, j]
+                    row_ind.append(node)
+                    col_ind.append(bottom_node)
+                    data.append(weight_bottom)
+
+                if i < rows - 1 and j < cols - 1:
+                    bottom_right_node = DurationExtractor._to_node_index(i + 1, j + 1, cols)
+                    weight_bottom_right = mat[i + 1, j + 1]
+                    row_ind.append(node)
+                    col_ind.append(bottom_right_node)
+                    data.append(weight_bottom_right)
+
+        adj_mat = coo_matrix((data, (row_ind, col_ind)), shape=(rows * cols, rows * cols))
+        return adj_mat.tocsr()
