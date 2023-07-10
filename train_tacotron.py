c8f77f090a09f9e65cc05ae2d439c89b00b12612
Christian Schäfer <54799962+cschaefer26@users.noreply.github.com>
2023-07-10 15:30:29 +0200

    Fix loading of taco dataset for duration extraction. (#102)

--- train_tacotron.py
+++ train_tacotron.py
@@ -179,19 +179,19 @@ if __name__ == '__main__':
     train_cfg = config['tacotron']['training']
     if args.force_gta:
         print('Creating Ground Truth Aligned Dataset...\n')
-        train_set, val_set = get_taco_dataloaders(paths.data, 1, model.r, max_mel_len=None)
+        train_set, val_set = get_taco_dataloaders(paths.data, 1, model.r, **train_cfg['filter'])
         create_gta_features(model, train_set, val_set, paths.gta)
         print('\n\nYou can now train WaveRNN on GTA features - use python train_wavernn.py --gta\n')
     elif args.force_align:
         print('Creating Attention Alignments and Pitch Values...')
-        train_set, val_set = get_taco_dataloaders(paths, 1, model.r, max_mel_len=None)
+        train_set, val_set = get_taco_dataloaders(paths, 1, model.r, **train_cfg['filter'])
         create_align_features(model=model, config=config,  paths=paths)
         print('\n\nYou can now train ForwardTacotron - use python train_forward.py\n')
     else:
         trainer = TacoTrainer(paths, config=config, dsp=dsp)
         trainer.train(model, optimizer)
         print('Training finished, now creating Attention Alignments and Pitch Values...')
-        train_set, val_set = get_taco_dataloaders(paths, 1, model.r, max_mel_len=None)
+        train_set, val_set = get_taco_dataloaders(paths, 1, model.r, **train_cfg['filter'])
         create_align_features(model=model,  config=config, paths=paths)
         print('\n\nYou can now train ForwardTacotron - use python train_forward.py\n')
 
